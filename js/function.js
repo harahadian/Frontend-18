a1015d4d479477c3bfcc2852cefcefe7e0411e38
harahadian <harahadian@gmail.com>
2023-12-13 11:26:09 +0700

    update

--- js/function.js
+++ js/function.js
@@ -1,33 +1,13 @@
-const API_BASE_URL = 'http://localhost:3300';
+const API_BASE_URL = 'http://localhost:3000';
 
-async function addcontact() {
-    const Customer_name = document.getElementById('name').value;
-    const Email = document.getElementById('email').value;
-    const Phone_Number = document.getElementById('phone').value;
-    const Subject = document.getElementById('subject').value;
-    const Message = documen.getElementById('message').value;
-    try {
-        await fetch (`${API_BASE_URL}/contact`, {
-            method : 'POST',
-            headers: {
-               'Content-Type':'application/json' 
-            },
-            body: JSON.stringify ({Customer_name, Email, Phone_Number, Subject, Message }),
-        });
-        alert("Submit is Successfully, Thankyou for your contact us")
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-async function addorder() {
+async function addmenu() {
     const Customer_name = document.getElementById('name').value;
     const Address = document.getElementById('address').value;
     const Phone_Number = document.getElementById('phone').value;
     const Order = document.getElementById('food').value;
-    const Order_Quantity = parseInt(document.getElementById('quantity').value);
+    const Order_Quantity = document.getElementById('quantity').value;
     try {
-        await fetch (`${API_BASE_URL}/order`, {
+        await fetch (`${API_BASE_URL}/menu`, {
             method : 'POST',
             headers: {
                'Content-Type':'application/json' 
--- js/message.js
+++ js/message.js
@@ -1,48 +1,8 @@
+const API_BASE_URL = 'http://localhost:3000';
 
-
-
-
-
-
-function addorder(event) {
-    event.preventDefault();
-    // const Noid = document.querySelector("#id");
-    const NamaPemesan = document.querySelector("name");
-    const AlamatPemesan = document.querySelector("address");
-    const NoTeleponPemesan = document.querySelector("phone");
-    const MakananPemesan = document.querySelector("food");
-    const JumlahPesanan = document.querySelector("quantity");
-  
-    fetch(`${BASE_URL}/InputOrder`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        // No_Id: Noid.value,
-        Customer_Name: NamaPemesan.value,
-        Address: AlamatPemesan.value,
-        Phone_Number: NoTeleponPemesan.value,
-        Order: MakananPemesan.value,
-        Order_Quantity: JumlahPesanan.value,
-      }),
-    }).then(response => response.json())
-      .then(response => {
-        alert(`Your Order Has Been Saved, Your Id Order Is ${response.No_Id}`);
-        // Noid.value = ''
-        NamaPemesan.value = '';
-        AlamatPemesan.value = '';
-        NoTeleponPemesan.value = '';
-        MakananPemesan.value = '';
-        JumlahPesanan.value = '';
-      }).catch((error) => {
-      console.error(error);
-    });
-  }
-  
-  function cekorder(event) {
-    const idOrder = document.querySelector("idOrder");
-    fetch(`${BASE_URL}/message/${idOrder.value}`)
+async function cekorder(event) {
+    const name = document.querySelector("name");
+    fetch(`${API_BASE_URL}/menu`)
     .then(response => response.json())
     .then(response => {
     if(response.data){
